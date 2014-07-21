3934f1a075ae27afe00252286f7b5905b906e2d0
rwilbert-digitec <rwilbert@digitecinteractive.com>
2014-07-21 16:45:57 -0400

    Update aglio.js
    changed rendering to  use a tempfile for large documetns

--- tasks/aglio.js
+++ tasks/aglio.js
@@ -5,17 +5,16 @@ var tmpFile = require('tempfile');
 var fs = require('fs');
 
 
-
-module.exports = function(grunt) {
+module.exports = function (grunt) {
     var _ = grunt.util._, aglio = require('aglio');
-    grunt.registerMultiTask('aglio', 'Grunt plugin to generate aglio documentation', function() {
+    grunt.registerMultiTask('aglio', 'Grunt plugin to generate aglio documentation', function () {
         var done = this.async();
         // Merge task-specific and/or target-specific options with these defaults.
 
         var default_options = {
             seperator: "",
-            theme: "default",
-            filter: function(src){
+            theme    : "default",
+            filter   : function (src) {
                 return src;
             }
         };
@@ -29,37 +28,36 @@ module.exports = function(grunt) {
 
         // Make sure that the given theme exists
         aglio.getTemplates(function (err, names) {
-            if(err){
+            if (err) {
                 grunt.log.warn(err);
             }
-            if(!_.contains(names, aglioOptions.template)){
+            if (!_.contains(names, aglioOptions.template)) {
                 // Is a custom theme file presented
                 aglioOptions.template = path.resolve(aglioOptions.template) + '.jade';
-                if(!grunt.file.exists(aglioOptions.template)) {
-                    grunt.log.warn(aglioOptions.template+" theme does not exist, reverting to the default theme");
+                if (!grunt.file.exists(aglioOptions.template)) {
+                    grunt.log.warn(aglioOptions.template + " theme does not exist, reverting to the default theme");
                     aglioOptions.template = "default";
                 }
             }
         });
 
 
-
-        this.files.forEach(function(f){
+        this.files.forEach(function (f) {
             var tempfile = tmpFile('asd');
-            f.src.filter(function(path){
-                if ( grunt.file.exists(path)){
-                    fs.appendFileSync(tempfile , grunt.file.read(path) )
+            f.src.filter(function (path) {
+                if (grunt.file.exists(path)) {
+                    fs.appendFileSync(tempfile, grunt.file.read(path))
                     fs.appendFileSync(tempfile, options.separator);
-                }else{
+                } else {
                     grunt.log.warn(path + " does not exist");
                 }
             });
-            aglio.renderFile(tempfile , f.dest , aglioOptions.template , function(err){
-                if ( err){
+            aglio.renderFile(tempfile, f.dest, aglioOptions.template, function (err) {
+                if (err) {
                     grunt.log.warn(err);
                 }
             });
-            grunt.log.ok("Written to " + f.dest )
+            grunt.log.ok("Written to " + f.dest)
         });
     }); //multitask
 
